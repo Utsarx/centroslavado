18ac09205afe0aeeddd204067615bc5b0bb543c0
Oswaldo Raúl Díaz Guzmán <oswaldo.dg@outlook.com>
2020-12-14 13:59:13 -0600

    Entidades Tipo de pago y UI

---
+++ backend/CL.Modelo/Empresa/MedioPagoEmpresa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CL.Modelo
+{
+    public class MedioPagoEmpresa
+    {
+        [Key]
+        public string MedioPagoId  { get; set; }
+        [Key]
+        public Guid EmpresaId { get; set; }
+
+        public EmpresaTransporte Empresa { get; set; }
+        public MedioPago MedioPago { get; set; }
+
+    }
+}
---
+++ backend/CL.Modelo/Tienda/MedioPago.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CL.Modelo
+{
+
+    public class MedioPago
+    {
+        // Este Id es de tipo string 
+        [Required]
+        [MaxLength(10)]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(250), MinLength(1)]
+        public string Nombre { get; set; }
+    }
+}
